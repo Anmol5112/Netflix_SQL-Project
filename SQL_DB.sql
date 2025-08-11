a7180d6af200004a337221438b05433eecad3ac1
Anmol Singh <114676719+Anmol5112@users.noreply.github.com>
2025-08-12 01:09:16 +0530

    Add files via upload

---
+++ SQL_DB.sql
@@ -0,0 +1,25 @@
+-- Netflix Project 
+DROP TABLE IF EXISTS netflix;
+CREATE TABLE  netflix
+(
+	show_id	VARCHAR(60),
+	type VARCHAR(10),
+	title	VARCHAR(150),
+	director	VARCHAR(208),
+	castS	VARCHAR(1000),
+	country	VARCHAR(150),
+	date_added VARCHAR(50),
+	release_year	INT,
+	rating	VARCHAR(10),
+	duration	VARCHAR(15),
+	listed_in	VARCHAR(150),
+	description		VARCHAR(250) 
+);
+
+SELECT * FROM netflix;
+
+
+
+
+
+
